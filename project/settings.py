61375557e71d1280eb5aec603ecc1635d500385e
Pawan Thakur <pwnkmrthkr@gmail.com>
2021-05-17 14:26:36 +0530

    cors headers added for accessing api from other website

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'rest_framework',
+    'corsheaders',
     'graphene_django',
 
 ]
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -189,3 +191,12 @@ REST_FRAMEWORK = {
 GRAPHENE = {
     "SCHEMA": "blog.schema.schema"
 }
+
+# CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    # "https://example.com",
+    # "https://sub.example.com",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:9000"
+    "http://localhost:3000"
+]
