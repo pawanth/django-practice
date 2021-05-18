70dbe70c0b156cad38ed02391dba5a37e5766c60
Pawan Kumar <pwnkmrthkr@gmail.com>
2021-05-18 19:37:11 +0530

    fixed os.getenv call

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv['SECRET_KEY', 'django-insecure-0d(r8dr3ubm)l$*!3s96)4oo2el8-uj4)yhx68#diih2pft2xg']
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-0d(r8dr3ubm)l$*!3s96)4oo2el8-uj4)yhx68#diih2pft2xg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('TESTING', True)
