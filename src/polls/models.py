d40c48357d74100ebd7bfe5607baadee79c9fb7e
Pawan Thakur <8505052+pawanth@users.noreply.github.com>
2024-01-06 21:36:02 +0530

    Updated admin interface a bit. (#1)
    nice work

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -13,6 +14,12 @@ class Question(models.Model):
 
     def __str__(self):
         return str(self.question_text)
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def was_published_recently(self):
         '''Check if question is recently published, i.e. within last 24 hours'''
